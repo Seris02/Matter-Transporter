9f003de70c6348fad5cf01746d761d407a29d507
Seris02 <littlenebulae421@gmail.com>
2023-07-03 17:12:03 +0800

    cleaned things up a bit, fixed some bugs, made the matter transporter show what block it's holding in the tooltip

--- src/main/java/com/seris02/mattertransporter/MatterTransporter.java
+++ src/main/java/com/seris02/mattertransporter/MatterTransporter.java
@@ -1,53 +1,14 @@
 package com.seris02.mattertransporter;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.Validate;
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
-import net.minecraft.server.packs.repository.FolderRepositorySource;
-import net.minecraft.server.packs.repository.PackSource;
-import net.minecraft.server.packs.repository.RepositorySource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.AddPackFindersEvent;
-import net.minecraftforge.event.OnDatapackSyncEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
-import net.minecraft.world.entity.player.Player;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -55,24 +16,16 @@ import net.minecraft.world.entity.player.Player;
 @EventBusSubscriber(modid = MatterTransporter.MODID, bus = Bus.MOD)
 public class MatterTransporter {
 	public static final  String MODID = "mattertransporter";
-	// Directly reference a slf4j logger
-	private static final Logger LOGGER = LogUtils.getLogger();
-	public static SimpleChannel channel = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> "v1.0", "v1.0"::equals, "v1.0"::equals);
+	public static final String VERSION = "v1.1";
+
+	public static SimpleChannel channel = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> VERSION, VERSION::equals, VERSION::equals);
 	
 	public MatterTransporter() {
 		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 		Content.register(modBus);
-		//Mod.EventBusSubscriber.Bus.MOD
-		//MinecraftForge.EVENT_BUS.addListener(this::onPlayerLeftClickBlock);
-		// Register the setup method for modloading
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
-	private void setup(final FMLCommonSetupEvent event) {
-		// some preinit code
-		//GoodPortals.channel.registerMessage(0, RefreshCamoModel.class, RefreshCamoModel::encode, RefreshCamoModel::decode, RefreshCamoModel::onMessage);
-	}
 }
